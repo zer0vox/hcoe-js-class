dc6dca13213b3077831cdb79cbbbfc626b892f01
Sumip Chaudhary <sumip780@gmail.com>
2024-06-27 23:10:47 +0545

    updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-firstName = prompt("Enter your first name: ");
+/*firstName = prompt("Enter your first name: ");
 lastName = prompt("Enter your last name: ");
 let formattedFirstName =
   firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
@@ -9,3 +9,7 @@ let formattedLastName =
 let fullName = formattedFirstName.concat(" ", formattedLastName);
 
 console.log(fullName);
+*/
+const num = prompt("Enter a number: ");
+const formattedNum = new Intl.NumberFormat("en-IN").format(num);
+console.log(formattedNum);
