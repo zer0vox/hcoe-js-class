df064fcecc4d4a594b08e235ba88246b19a0f2b1
Sumip Chaudhary <sumip780@gmail.com>
2024-06-27 14:20:01 +0545

    Updated documents

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const gender = prompt("Please enter your gender (male or female):");
+//const gender = prompt("Please enter your gender (male or female):");
 //three equality for strict equality and coercion ie use === check data typr , value and give boolean value
 /*if (gender === "male") {
     alert(`you are ${gender}`);
@@ -6,9 +6,46 @@ const gender = prompt("Please enter your gender (male or female):");
     alert(`you are ${gender}`);
 } else {
     alert("Welcome!");
-}*/
+
 gender === "male"
   ? alert(`you are ${gender}`)
   : gender === "female"
   ? alert(`you are ${gender}`)
   : alert("others");
+
+switch (gender) {
+  case "male":
+    alert(`you are ${gender}`);
+    break;
+  case "female":
+    alert(`you are ${gender}`);
+    break;
+  default:
+    alert("others");
+}
+const marks = prompt("Please enter your marks:");
+
+switch (true) {
+  case marks > 80:
+    alert("A");
+    break;
+  case marks >= 60 && marks <= 80:
+    alert("B");
+    break;
+  default:
+    alert("C");
+    break;
+}
+
+alert(`Your grade is ${grade}`);
+*/
+//write a js program to ask user for username and password and welcome only if username === password
+const name = prompt("Please enter your name:");
+const username = prompt("Please enter your username:");
+const password = prompt("Please enter your password:");
+
+if (username === name && password === name) {
+  alert("Welcome!" + name);
+} else {
+  alert("Incorrect username or password.");
+}
