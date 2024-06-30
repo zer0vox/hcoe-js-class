33b970f5c5753db88adb93ad4a2e77b36aa72206
Sumip Chaudhary <sumip780@gmail.com>
2024-06-30 14:35:32 +0545

    Ending js

--- index.js
+++ index.js
@@ -62,3 +62,112 @@ console.log(sum(110, 200));
 //     return model + this.name;
 //   },
 // };
+//2
+
+// const prod = {
+//   name: "headphones",
+//   price: 83.7,
+//   discount: "7%",
+// };
+
+// const getNewProduct = (product) => {
+//   const { price, discount, ...rest } = product;
+//   const discAmt = price > 100 ? 0.07 * price : 0.1 * price;
+//   rest.price = price - discAmt;
+//   rest.discount = price > 100 ? "10%" : "7%";
+//   return rest;
+// };
+// const newProd = getNewProduct(prod);
+// console.log(newProd);
+// let flist1 = ["Noodle", "Pasta", "Ice-cream"];
+// let flist2 = ["Fries", "Ice-cream", "Pizza"];
+// const findcommon = (flist1, flist2) => {
+//   return flist1.filter((item) => flist2.includes(item));
+// };
+// console.log(findcommon(flist1, flist2));
+// const classes = [
+//   { name: "Ram", age: 20 },
+//   { name: "Sita", age: 15 },
+//   { name: "Shyam", age: 40 },
+// ];
+
+// const agesorting = (classes) => {
+//   return classes.sort((a, b) => a.age - b.age);
+// };
+
+// // console.log(agesorting(classes));
+// const countries = ["Nepal", "USA", "Australia"];
+
+// const findMaxLength = (countries) => {
+//   const lengths = countries.map((country) => country.length);
+//   const maxLength = Math.max(...lengths);
+//   const maxLengthIndex = lengths.indexOf(maxLength);
+//   return countries[maxLengthIndex];
+// };
+
+// console.log(findMaxLength(countries));
+
+// const findcommon = (flist1, flist2) => {
+//   for (i = 0; i < flist1.length; i++) {
+//     for (j = 0; j < flist2.length; j++) {
+//       if (flist1[i] === flist2[j]) return true;
+//     }
+//   }
+//   return false;
+// };
+
+// let flist1 = ["admin", "manager"];
+// let flist2 = ["user", "receptionist", "manager"];
+
+// console.log(findcommon(flist1, flist2));
+
+// const findcommon = (flist1, flist2) => {
+//   return flist1.some(item => flist2.includes(item));
+// };
+
+// let flist1 = ["admin", "manager"];
+// let flist2 = ["user", "receptionist", "manager"];
+
+// console.log(findcommon(flist1, flist2));
+//Get the total number of characters by eye color (hint. a map of eye color to count)
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+const eyeColorCount = characters.reduce((accumulator, character) => {
+  const { eye_color } = character;
+  if (accumulator[eye_color]) {
+    accumulator[eye_color]++;
+  } else {
+    accumulator[eye_color] = 1;
+  }
+  return accumulator;
+}, {});
+
+console.log(eyeColorCount);
